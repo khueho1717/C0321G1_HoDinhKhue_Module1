b26e1fdcea966833374908f7c55889a05ad5c477
hyuganejidn@gmail.com <xuan hung>
2021-04-02 13:47:39 +0700

    bai7

---
+++ _07_bien_kieu_du_lieu_va_toan_tu/thuchanh/su_dung_toan_tu.js
@@ -0,0 +1,9 @@
+
+let inputWidth;
+let inputHeight;
+inputWidth=prompt("enter the width: ");
+inputHeight=prompt("enter the height: ");
+Width=parseInt(inputWidth);
+Height=parseInt(inputHeight);
+let area=Width*Height;
+document.write("the area is "+ area);
